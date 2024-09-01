264ec81f4e10b6b1c578449fd89c078777b81618
Ankit <as400042@gmail.com>
2024-09-01 17:23:53 +0530

    test

--- providers.tf
+++ providers.tf
@@ -1,5 +1,3 @@
-variable "client_secret" {
-}
 
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
@@ -25,8 +23,8 @@ provider "azapi" {
 provider "azurerm" {
   features {}
 
-  client_id       = "d83cb496-a471-4df9-9b85-a0ccf46f2257" 
-  client_secret   = "9pe8Q~opA2Ba01cQadpfuKlArRr9RXe.GNDPAb5b"
-  tenant_id       = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
-  subscription_id = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
+  client_id       = "var.clientid"
+  client_secret   = "var.clientsecret"
+  tenant_id       = "var.tenantid"
+  subscription_id = "var.subid"
 }
