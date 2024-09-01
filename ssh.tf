a687a0ec43170baa8fadee9e2ee86443903fc682
Ankit <as400042@gmail.com>
2024-09-01 17:46:21 +0530

    test

--- ssh.tf
+++ ssh.tf
@@ -3,22 +3,15 @@ resource "random_pet" "ssh_key_name" {
   separator = ""
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
+resource "azurerm_ssh_public_key" "ssh_key" {
+  name                = random_pet.ssh_key_name.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-  response_export_values = ["publicKey", "privateKey"]
-}
-
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
-  parent_id = azurerm_resource_group.rg.id
+  depends_on = [azurerm_resource_group.rg]
 }
 
 output "key_data" {
-  value = azapi_resource_action.ssh_public_key_gen.output.publicKey
+  value = azurerm_ssh_public_key.ssh_key.public_key
 }
+
