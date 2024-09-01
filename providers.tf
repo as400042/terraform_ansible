f36121ac50a703aa0a020588df08bb1fedb8c6f1
Ankit <as400042@gmail.com>
2024-09-01 17:15:25 +0530

    test

--- providers.tf
+++ providers.tf
@@ -25,8 +25,8 @@ provider "azapi" {
 provider "azurerm" {
   features {}
 
-  client_id       = "var.clientid"
-  client_secret   = "var.clientsecret"
-  tenant_id       = "var.tenantid"
-  subscription_id = "var.suobid"
+  client_id       = var.clientid
+  client_secret   = var.clientsecret
+  tenant_id       = var.tenantid
+  subscription_id = var.suobid
 }
